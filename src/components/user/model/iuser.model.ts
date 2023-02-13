cb145804944b7a61643f3e22b74eef2cbddd3bba
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-02-13 10:13:10 +0700

    [ADD] User Interface

---
+++ src/components/user/model/iuser.model.ts
@@ -0,0 +1,13 @@
+import { Document } from "mongoose";
+import { role } from "../utils/user.role";
+import { status } from "../utils/user.status";
+
+export default interface IUser extends Document {
+  username: string;
+  password: string;
+  name: string;
+  role: role;
+  status: status;
+  teams: string[];
+  isDeleted: boolean;
+}
---
+++ src/components/user/utils/user.role.ts
@@ -0,0 +1,5 @@
+export enum role {
+  admin = "Admin",
+  leader = "Leader",
+  member = "Member",
+}
