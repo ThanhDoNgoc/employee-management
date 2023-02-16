6b6f28d2af1235b16c5c5af3f5c5b5064dd9fd54
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-02-16 12:30:54 +0700

    [MOD] change team return data type

--- src/components/team/utils/team.return.data.ts
+++ src/components/team/utils/team.return.data.ts
@@ -6,3 +6,22 @@ export default interface ITeamReturnData {
   leaderId: Schema.Types.ObjectId | null;
   members: Schema.Types.ObjectId[];
 }
+
+export interface ITeamsReturnData {
+  _id?: Schema.Types.ObjectId;
+  name: string;
+  leader: IMember | null;
+}
+
+export interface ITeamDetailReturnData {
+  _id?: Schema.Types.ObjectId;
+  name: string;
+  leader: IMember | null;
+  members: IMember[];
+}
+
+export interface IMember {
+  _id?: Schema.Types.ObjectId;
+  name: string;
+  username: string;
+}
